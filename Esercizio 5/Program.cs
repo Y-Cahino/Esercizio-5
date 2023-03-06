a3485a9b888b6112330ba8e279b3629df429b69b
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-03-06 14:43:03 +0100

    percentuale

--- Esercizio 5/Program.cs
+++ Esercizio 5/Program.cs
@@ -30,6 +30,9 @@ namespace Esercizio_5
                 }
             }
             Console.WriteLine("I numeri positivi sono: "+pos+"\nI numeri negativi sono: "+neg);
+            int ppos = (pos*100)/d;
+            int npos = (neg*100)/d;
+            Console.WriteLine("La percentuale di numeri pos è "+pos+"\nMentre percentuale numeri neg è "+neg);
         }
     }
 }
